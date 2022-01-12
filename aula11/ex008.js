d6b59ff6b66ff78279dacf89ab05c2801badfe6d
Diego Araujo <97152724+diegoarauj0@users.noreply.github.com>
2022-01-11 22:57:46 -0300

    .

---
+++ aula11/ex008.js
@@ -0,0 +1,4 @@
+var idade = 19
+if (idade >= 18){
+    console.log('vc e maio de idade')
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var idade = 11
+if (idade >= 18){
+    console.log('vc e maio de idade')
+}
+else {
+    console.log('vc e menor de idade')
+}
